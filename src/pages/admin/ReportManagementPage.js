877cca90194b117f72ba36fe71721f08a7be8082
yudahey0826 <yudahye0826@naver.com>
2025-04-28 23:12:55 +0900

    ㅅㅈㅈ

--- src/pages/admin/ReportManagementPage.js
+++ src/pages/admin/ReportManagementPage.js
@@ -61,6 +61,7 @@ function ReportManagement() {
             author: response.data.userName,
             createdAt: response.data.createdAt,
           })
+
           break
 
         case "VISITEDPLACE":
